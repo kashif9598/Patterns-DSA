d9f94e0fb498c5a6618a44a3a1247397d1bb9804
kashif9598 <kashifsheikh9598@gmail.com>
2023-04-16 18:06:29 +0530

    Pattern 4 - 7

---
+++ pattern5.js
@@ -0,0 +1,31 @@
+// Geek is very fond of patterns. Once, his teacher gave him a pattern to solve. He gave Geek an integer n and asked him to build a pattern.
+
+// Help Geek build a star pattern.
+// Example 1:
+
+// Input: 5
+
+// Output:
+// * * * * *
+// * * * * 
+// * * * 
+// * *  
+// * 
+// Your Task:
+
+// You don't need to input anything. Complete the function printTriangle() which takes  an integer n  as the input parameter and print the pattern.
+// Constraints:
+// 1<= N <= 20
+
+let pattern = "";
+const printTriangle = (num) => {
+    for (let i = 1; i <= num; i++) {
+        for (let j = num; j >= i; j--) {
+            pattern += "*";
+        }
+        pattern += "\n"
+    }
+    return pattern;
+}
+
+console.log(printTriangle(5))
